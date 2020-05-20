7884346c2738c302a67319771a1b3434c0e1c1f2
WojtekOgorek <ogorek.wojciech@gmail.com>
2020-05-20 21:36:18 +0200

    Service -> collection from json method created and main map method added

--- service/src/main/java/ogorek/wojciech/service/ShoppingService.java
+++ service/src/main/java/ogorek/wojciech/service/ShoppingService.java
@@ -1,12 +1,18 @@
 package ogorek.wojciech.service;
 
+import ogorek.wojciech.persistence.converter.JsonClientWithProductsConverter;
+import ogorek.wojciech.persistence.exception.AppException;
 import ogorek.wojciech.persistence.model.Client;
 import ogorek.wojciech.persistence.model.ClientWithProducts;
 import ogorek.wojciech.persistence.model.Product;
+import ogorek.wojciech.persistence.validator.impl.ClientWithProductsValidator;
 
 
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ShoppingService {
 
@@ -17,13 +23,45 @@ public class ShoppingService {
         clientsWithProducts = getCustomersWithOrders(jsonFilenames);
     }
 
-    //TODO
+
     private Set<ClientWithProducts> readClientsFromJsonFile(Set<String> jsonFilenames){
-        return null;
+        var clientWithProductsValidator = new ClientWithProductsValidator();
+        var counter = new AtomicInteger(1);
+
+        return jsonFilenames
+                .stream()
+                .flatMap(jsonFilename ->
+                        new JsonClientWithProductsConverter(jsonFilename)
+                                .fromJson()
+                                .orElseThrow(() -> new AppException("shopping service - cannot read data from json file"))
+                                .stream()
+                                .filter(client -> {
+                                    var errors = clientWithProductsValidator.validate(client);
+                                    if (clientWithProductsValidator.hasErrors()) {
+                                        System.out.println("\n-------------- validation errors for client no. " + counter.get() + " in file " + jsonFilename + " ----------");
+                                        errors.forEach((k, v) -> System.out.println(k + ": " + v));
+                                        System.out.println("\n\n");
+                                    }
+                                    counter.incrementAndGet();
+                                    return !clientWithProductsValidator.hasErrors();
+                                }))
+                .collect(Collectors.toSet());
     }
 
-    //TODO
+
     private Map<Client, Map<Product,Long>> getCustomersWithOrders(Set<String> jsonFilenames){
-        return null;
+        Map<Client, Map<Product, Long>> m1 = readClientsFromJsonFile(jsonFilenames)
+                .stream()
+                .collect(Collectors.groupingBy(
+                        ClientWithProducts::getClient,
+                        Collectors.collectingAndThen(
+                                Collectors.flatMapping(cwp -> cwp.getProducts().stream(), Collectors.toList()),
+                                items -> items
+                                        .stream()
+                                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                        )
+                ));
+        System.out.println(m1);
+        return m1;
     }
 }
